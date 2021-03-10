2eac58bed76f83f49182a9401a3ad44f8b12692c
Ваше Имя <you@example.com>
2021-03-10 23:25:52 +0200

    finish

--- index.js
+++ index.js
@@ -2,7 +2,27 @@ const csv = require('csv-parser')
 const fs = require('fs')
 const results = [];
 
-// 1. import library
+
+const unique = (arr) =>{
+    const uniqueEl = [];
+    for(let i = 0; i < arr.length; i++){
+        if(!uniqueEl.includes(arr[i])){
+            uniqueEl.push(arr[i])
+        }
+    }
+    return uniqueEl
+}
+const reformatDate = (oldDate) =>{
+    const unixTimestamp = Date.parse(oldDate)
+    const date = new Date (unixTimestamp)
+    const day = date.getDate().toString().padStart(2, '0')
+    const month = (date.getMonth()+1).toString().padStart(2, '0')
+    const year = date.getFullYear()
+    return year + '-' + month + '-' + day
+
+
+}
+
 const csvWriterLibrary = require('csv-writer')
 
 fs.createReadStream('./file/acme_worksheet.csv')
@@ -12,22 +32,42 @@ fs.createReadStream('./file/acme_worksheet.csv')
 
     })
     .on('end', () => {
-        console.log(results);
 
+        const employeeNames = results.map(item => item['Employee Name'])
+        const uniqueEmployeeNames = unique(employeeNames)
+
+        const dates = results.map(item=> item.Date)
+        const uniqueDates = unique(dates)
 
         const header = [
-            {id: 'Employee Name', title: 'NAME / DATE'},
-            {id: 'Date', title: results.map(date => date.Date)},
+            {id: 'name', title: 'Name / Date'},
+
         ]
 
+        for (let i = 0; i < uniqueDates.length; i++){
+            header.push({id: i.toString(), title: reformatDate(uniqueDates[i])})
+        }
 
         const objectCsvWriter = csvWriterLibrary.createObjectCsvWriter({
             path: './file/output.csv',
             header: header
         });
+            const output = []
+            for(let i = 0; i < uniqueEmployeeNames.length; i++){
+                const row = {name: uniqueEmployeeNames[i]}
+                for(let j = 0; j < uniqueDates.length; j++){
+                    const result = results.find(item => item['Employee Name'] === uniqueEmployeeNames[i] && item.Date === uniqueDates[j] )
+                    if(result){
+                        row[j.toString()] = result['Work Hours']
+                    }else {
+                        row[j.toString()] = 0
+                    }
 
+                }
+                output.push(row)
+            }
 
-        objectCsvWriter.writeRecords(results)
+        objectCsvWriter.writeRecords(output)
             .then(() => {
                 console.log('...Done');
             });
